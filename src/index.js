ba8486291e9d213d385a60ec0e07ac09fe8214f2
China-forrest-w <linhaodewell@163.com>
2021-03-28 18:23:15 +0800

    feat(router/route): 实现Router&Route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import User from './components/User';
 
 ReactDOM.render(
-  <React.StrictMode>
-    router
-  </React.StrictMode>,
+  <HashRouter>
+    <Route exact={false} path="/" component={Home} />
+    <Route path="/user" component={User} />
+    <Route path="/profile" component={Profile} />
+
+  </HashRouter>,
   document.getElementById('root')
 );
+/* 
+路由容器：Router
+路由规则：Route
+*/
---
+++ src/react-router/Route.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import RouterContext from './RouterContext';
+
+/* 
+获取到context中的值
+*/
+
+class Route extends React.Component {
+    static contextType = RouterContext;
+    render() {
+        const { history, location } = this.context;
+        const { path, component: RouteComponent } = this.props;
+        const match = location.pathname === path;
+        let renderElement = null;
+        let routeProps = { history, location, match };
+        if (match) {
+            renderElement = <RouteComponent {...routeProps} />
+        }
+        return renderElement;
+    }
+}
+
+export default Route;
